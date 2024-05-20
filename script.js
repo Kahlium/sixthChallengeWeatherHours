feca03ac9c4bdf59aafb3553ba1436b48a6d9616
Kahlium <kahligtz@gmail.com>
2024-05-20 17:25:55 -0600

    temperature fetched + in celsius now

--- script.js
+++ script.js
@@ -2,12 +2,12 @@
 const city = document.getElementById("city")
 const weatherButton = document.getElementById("getWeather")
 
-const temp = document.getElementById("temperature")
+const temperature = document.getElementById("temperature")
 const weather = document.getElementById("weather")
 
 function weatherFetch() {
     let cityName = localStorage.getItem('city')
-    fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${cityName}&appid=cb3d45c4f69eaeaed96e4308e4f6d9cb`)
+    fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${cityName}&units=metric&appid=cb3d45c4f69eaeaed96e4308e4f6d9cb`)
         .then(function (response) {
             return response.json();
         })
@@ -15,6 +15,7 @@ function weatherFetch() {
             console.log(data);
 
             for (let i = 0; i < 5; i++) {
+                temperature.textContent = data.list[i].main.temp
                 weather.textContent = data.list[i].weather[0].main;
                 console.log(data.list[i].weather[0].main);
               }
