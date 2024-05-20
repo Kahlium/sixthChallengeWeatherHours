7ae8acb476849ed8470c6091e8d2454f88b1e324
Kahlium <kahligtz@gmail.com>
2024-05-20 17:09:13 -0600

    fetching weather now, figuring out how to fetch everything else

--- script.js
+++ script.js
@@ -2,6 +2,9 @@
 const city = document.getElementById("city")
 const weatherButton = document.getElementById("getWeather")
 
+const temp = document.getElementById("temperature")
+const weather = document.getElementById("weather")
+
 function weatherFetch() {
     let cityName = localStorage.getItem('city')
     fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${cityName}&appid=cb3d45c4f69eaeaed96e4308e4f6d9cb`)
@@ -10,6 +13,11 @@ function weatherFetch() {
         })
         .then(function (data) {
             console.log(data);
+
+            for (let i = 0; i < 5; i++) {
+                weather.textContent = data.list[i].weather[0].main;
+                console.log(data.list[i].weather[0].main);
+              }
         });
 }
 
