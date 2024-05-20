488fd4cd228880abcb2aa45a5f6f76b163737ab4
Kahlium <kahligtz@gmail.com>
2024-05-20 16:47:28 -0600

    fetching city works now

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
 //API key cb3d45c4f69eaeaed96e4308e4f6d9cb
+const city = document.getElementById("city")
+const weatherButton = document.getElementById("getWeather")
+
+function weatherFetch() {
+    let cityName = localStorage.getItem('city')
+    fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${cityName}&appid=cb3d45c4f69eaeaed96e4308e4f6d9cb`)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            console.log(data);
+        });
+}
+
+weatherButton.addEventListener("click", function(event)
+{
+    event.preventDefault();
+    localStorage.setItem("city", city.value)
+
+    weatherFetch();
+});
